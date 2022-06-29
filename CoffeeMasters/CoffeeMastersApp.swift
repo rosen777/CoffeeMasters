57380bad4036a7939e81a651f689cfb745ede9a0
Rosen Toshev <rtoscheff@gmail.com>
2022-06-29 22:30:44 +0100

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Rosen Toshev on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeMasters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeMasters
+//
+//  Created by Rosen Toshev on 29/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
